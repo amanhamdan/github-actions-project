27cc574ef378dd3fcfe4680989bfcfda7d8cf2e0
amanhamdan <aman.hamdan@contractoralefeducation.com>
2024-03-01 00:22:42 +0400

    code modified

--- src/main/java/com/javaaman/githubcicdactions/GithubCicdActionsApplication.java
+++ src/main/java/com/javaaman/githubcicdactions/GithubCicdActionsApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class GithubCicdActionsApplication {
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "ELCOMEEEEE";
+		return "ELhey COMEEEEE";
 	}
 //	echo "# github-actions-project" >> README.md
 //	git init
