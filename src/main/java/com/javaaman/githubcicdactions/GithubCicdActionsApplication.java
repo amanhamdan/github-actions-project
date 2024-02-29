20fcb4bdb8422a7de11f10eed9b6e6d5159e2143
amanhamdan <aman.hamdan@contractoralefeducation.com>
2024-02-29 14:05:08 +0400

    code

---
+++ src/main/java/com/javaaman/githubcicdactions/GithubCicdActionsApplication.java
@@ -0,0 +1,27 @@
+package com.javaaman.githubcicdactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "ELCOMEEEEE";
+	}
+//	echo "# github-actions-project" >> README.md
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin https://github.com/amanhamdan/github-actions-project.git
+//	git push -u origin main
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
